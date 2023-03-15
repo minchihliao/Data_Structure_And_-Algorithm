7444a45234c1b07167876af57cc6c7e10eee4902
minchihliao <xicoo0730@gmail.com>
2023-03-15 13:33:54 +0800

    Implement DoubleLinkList

---
+++ Double_LinkList/DoubleLinkList.cs
@@ -0,0 +1,79 @@
+namespace DataStructure
+{
+    class DoubleNode{
+        public int Data;
+        public DoubleNode Prev;
+        public DoubleNode Next;
+        public DoubleNode(int value){
+            this.Data = value;
+            this.Prev = null;
+            this.Next = null;
+        }
+    }
+
+    class MyDoubleLinkList{
+        private DoubleNode head;
+        public DoubleNode Head{get{return head;}}
+
+        public void Push(int newValue){
+            DoubleNode newNode = new DoubleNode(newValue);
+            newNode.Next = head;
+            if(head!=null)
+                head.Prev = newNode;
+            head = newNode;
+        }
+
+        public void Insert(DoubleNode prevNode,int newValue){
+            if(prevNode==null)
+                Console.WriteLine("The Node is null");
+            DoubleNode newNode = new DoubleNode(newValue);
+            newNode.Next = prevNode.Next;
+            newNode.Prev = prevNode;
+            prevNode.Next = newNode;
+
+            if(newNode.Next!=null)
+                newNode.Next.Prev = newNode;
+        }
+
+        public void Append(int newValue){
+            DoubleNode newNode = new DoubleNode(newValue);
+            if(head ==null){
+                head = newNode;
+                return;
+            }
+            DoubleNode last = head;
+            
+            while(last.Next!=null){
+                last = last.Next;
+            }
+            last.Next = newNode;
+            newNode.Prev = last;
+        }
+
+        public void DeleteNode(DoubleNode targetNode){
+            if(targetNode==null||head ==null)
+                return;
+
+            if(head==targetNode){
+                head = targetNode.Next;
+            }
+
+            if(targetNode.Next!=null){
+                targetNode.Next.Prev = targetNode.Prev;
+            }
+
+            if(targetNode.Prev!=null){
+                targetNode.Prev.Next = targetNode.Next;
+            }
+        }
+
+        public void PrintList(){
+            DoubleNode temp = head;
+            while(temp!=null){
+                Console.Write(temp.Data + " ");
+                temp = temp.Next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Test.cs
+++ Test.cs
@@ -46,5 +46,44 @@ namespace DataStructure{
                         + "Insert 9 at Position 2-3: ");
             llist.PrintList();
         }
+
+        public void MyDoubleLinkList(){
+            MyDoubleLinkList dll = new MyDoubleLinkList();
+
+            
+
+            // Insert 2. So linked list becomes 2->NULL
+            dll.Push(2);
+
+            // Insert 6. So linked list becomes 2->6->NULL
+            dll.Append(6);
+
+            // Insert 4. So linked list becomes 4->2->6->NULL
+            dll.Push(4);
+  
+            // Insert 8. So linked list becomes 8->4->2->6->NULL
+            dll.Push(8);
+  
+            // Insert 10. So linked list becomes 10->8->4->2->6->NULL
+            dll.Push(10);
+  
+            // Insert 8, after 7. So linked list
+            // becomes 10->8->8->4->2->6->NULL
+            dll.Insert(dll.Head.Next, 8);
+
+
+            Console.Write("Original Linked list ");
+            dll.PrintList();
+  
+            // Deleting first node
+            dll.DeleteNode(dll.Head);
+  
+            dll.DeleteNode(dll.Head.Next);
+  
+            dll.DeleteNode(dll.Head.Next);
+    
+            Console.Write("Modified Linked list ");
+            dll.PrintList();
+        }
     }
 }
